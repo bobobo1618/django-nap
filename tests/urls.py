3382682d0481be8e8ceb18af45fb7c0cca02ed7f
funkybob <curtis@tinbrain.net>
2014-11-26 15:40:57 +1100

    Test assumes no autodiscover

--- tests/urls.py
+++ tests/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include
 from . import rpc_views
 
 from nap.rest import api
-api.autodiscover()
+#api.autodiscover()
 
 urlpatterns = patterns('',
     (r'^api/', include(api.patterns(True))),
